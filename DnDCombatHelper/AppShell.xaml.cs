f3117bb0dbade8717ebc62cde55e474ad08162a7
Andrei Pop <irewiewer@gmail.com>
2022-04-05 10:21:49 +0300

    [+] Initial Xamarin Cross-platform project

---
+++ DnDCombatHelper/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using DnDCombatHelper.ViewModels;
+using DnDCombatHelper.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace DnDCombatHelper
+{
+	public partial class AppShell : Xamarin.Forms.Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+			Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+			Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+		}
+
+	}
+}
---
+++ DnDCombatHelper/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DnDCombatHelper.Models;
+using DnDCombatHelper.ViewModels;
+using DnDCombatHelper.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DnDCombatHelper.Views
+{
+	public partial class ItemsPage : ContentPage
+	{
+		ItemsViewModel _viewModel;
+
+		public ItemsPage()
+		{
+			InitializeComponent();
+
+			BindingContext = _viewModel = new ItemsViewModel();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			_viewModel.OnAppearing();
+		}
+	}
+}
