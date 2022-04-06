b000982f2cd2a43d4f523dadc7dec28e8e644c0c
Andrei Pop <irewiewer@gmail.com>
2022-04-06 07:52:07 +0300

    [+] Added WinForms PC version

---
+++ DnDCombatHelperPC/MainView.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace DnDCombatHelperPC
+{
+	partial class MainView
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Enabled = false;
+			this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.textBox1.Location = new System.Drawing.Point(90, 9);
+			this.textBox1.MaxLength = 4;
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(80, 31);
+			this.textBox1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(25)))), ((int)(((byte)(53)))));
+			this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label1.ForeColor = System.Drawing.SystemColors.Control;
+			this.label1.Location = new System.Drawing.Point(10, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(83, 31);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Code:";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(25)))), ((int)(((byte)(53)))));
+			this.button1.Enabled = false;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(-1, -13);
+			this.button1.Margin = new System.Windows.Forms.Padding(0);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(187, 68);
+			this.button1.TabIndex = 2;
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// MainView
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(5)))), ((int)(((byte)(22)))));
+			this.ClientSize = new System.Drawing.Size(799, 450);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainView";
+			this.Text = "D&D Combat Helper";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private TextBox textBox1;
+		private Label label1;
+		private Button button1;
+	}
+}
---
+++ DnDCombatHelperPC/MainView.cs
@@ -0,0 +1,20 @@
+﻿namespace DnDCombatHelperPC
+{
+	public partial class MainView : Form
+	{
+		public MainView()
+		{
+			InitializeComponent();
+			Random rng = new Random();
+
+			// 4 random digits
+			textBox1.Text = rng.Next(0, 9).ToString() +
+							rng.Next(0, 9).ToString() +
+							rng.Next(0, 9).ToString() +
+							rng.Next(0, 9).ToString();
+
+			// maybe this?
+			// https://stackoverflow.com/questions/36823404/server-client-application-with-net-and-xamarin
+		}
+	}
+}
